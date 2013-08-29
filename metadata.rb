d08b2aec1cf9683d94c51d63a46f1d715f863a38
Tonis Tiigi <tonistiigi@gmail.com>
2013-08-29 17:03:43 +0300

    Remove old attributes

--- metadata.rb
+++ metadata.rb
@@ -8,15 +8,12 @@ name             "nodejs"
 provides         "nodejs"
 
 recipe "nodejs", "Installs Node.JS based on the default installation method"
-recipe "nodejs::install_from_source", "Installs Node.JS from source"
-recipe "nodejs::install_from_binary", "Installs Node.JS from official binaries"
-recipe "nodejs::install_from_package", "Installs Node.JS from packages"
-recipe "nodejs::npm", "Installs npm from source - a package manager for node"
 
-%w{ apt yum build-essential }.each do |c|
+
+%w{ build-essential }.each do |c|
   depends c
 end
 
 %w{ debian ubuntu centos redhat smartos }.each do |os|
-    supports os
+  supports os
 end
